0d2221b1982138b903f7c899245a863f2ab34fcf
OliOBr <oli.obrien@softwire.com>
2021-07-05 17:14:34 +0100

    FizzBuzz most basic version

--- app/src/main/java/com/example/fizzbuzz/FizzBuzz.kt
+++ app/src/main/java/com/example/fizzbuzz/FizzBuzz.kt
@@ -1,2 +1,20 @@
 package com.example.fizzbuzz
 
+fun main() {
+    val range = 1..100
+    for (number in range){
+        if ((number % 15) == 0) {
+            print("FizzBuzz ");
+        } else {
+            if ((number % 5) == 0){
+                print("Buzz ");
+            } else {
+                if (number % 3 == 0) {
+                    print("Fizz ")
+                } else {
+                    print("$number ")
+                }
+            }
+        }
+    }
+}
