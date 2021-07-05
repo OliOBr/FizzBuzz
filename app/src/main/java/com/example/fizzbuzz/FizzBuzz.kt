c70e53b9121a6bdbab6ab9d8f654520f8a1542fb
OliOBr <oli.obrien@softwire.com>
2021-07-05 17:20:55 +0100

    FizzBuzz added bangs for 7

--- app/src/main/java/com/example/fizzbuzz/FizzBuzz.kt
+++ app/src/main/java/com/example/fizzbuzz/FizzBuzz.kt
@@ -3,18 +3,19 @@ package com.example.fizzbuzz
 fun main() {
     val range = 1..100
     for (number in range){
-        if ((number % 15) == 0) {
-            print("FizzBuzz ");
-        } else {
-            if ((number % 5) == 0){
-                print("Buzz ");
-            } else {
-                if (number % 3 == 0) {
-                    print("Fizz ")
-                } else {
-                    print("$number ")
-                }
-            }
+        var output = ""
+        if (number % 3 == 0){
+            output += "Fizz"
         }
+        if (number % 5 == 0){
+            output += "Buzz"
+        }
+        if (number % 7 == 0){
+            output += "Bang"
+        }
+        if (output.isEmpty()){
+            output = number.toString()
+        }
+        print(output + "\n")
     }
 }
